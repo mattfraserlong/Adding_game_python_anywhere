91fcc2052e91e86f58b2f5f83c57b6b9e438478d
mattfraserlong <mattfraserlong@gmail.com>
2016-03-28 22:15:31 +0100

    Adding_game
    Adding_game as uploaded by GitHub Desktop

---
+++ bin/app.py
@@ -0,0 +1,82 @@
+import web
+from random import randint
+
+urls = (
+    '/', 'Index',
+    '/dummy', 'Dummy',
+    '/addanswer', 'Addanswer'
+)
+
+target = []
+sum_answer = 0
+rand_one_add = 0
+rand_two_add = 0
+count = 0
+size = 0
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/', globals={
+    'randint': randint,
+    'target': target,
+    'str': str,
+    'sum_answer': sum_answer,
+    'rand_one_add': rand_one_add,
+    'rand_two_add': rand_two_add,
+    'count': count,
+    'size': size
+
+})
+
+class Index(object):
+    def GET(self):
+        return render.game_start()      
+        
+    def POST(self):
+        global count
+        form = web.input(name="choice")
+        if form.choice == "a":
+            return render.addition()
+        elif form.choice == "s":
+            return render.subtraction()
+        elif form.choice == "m":
+            return render.multiplication()
+        elif form.choice == "d":
+            return render.division()
+        elif form.choice == "Yes":
+            count = 0
+            return render.game_start()
+        else:
+            return render.dummy()
+            
+class Dummy(object):
+    def POST(self):
+        global count
+        global size
+        global target
+        form = web.input(name="choice")  
+        if form.choice == "Yes":
+            count += 1
+            return render.game_start()
+        elif form.choice == "No":
+            size -= 1
+            return render.game_over2()
+        else:
+            return render.dummy()
+            
+class Addanswer(object):
+    def POST(self):
+        form = web.input(name="addchoice")
+        if form.addchoice == str(target[count]):
+            return render.congrats()
+        else:
+            return render.game_over()
+
+# To do: extend to all operators
+# To do: Host on real server
+
+
+if __name__ == "__main__":
+
+    app.run()
+
---
+++ bin/apptest.py
@@ -0,0 +1,3 @@
+from sys import argv
+
+script, data, ty = argv
